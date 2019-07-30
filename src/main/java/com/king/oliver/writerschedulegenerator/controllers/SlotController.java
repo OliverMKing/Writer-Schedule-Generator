1517c25f6f1728db426e46bf19dfc47a2dc56d5b
Oliver King <33605867+OliverMKing@users.noreply.github.com>
2019-07-30 17:52:37 -0400

    Added update functionality to SlotController and views

--- src/main/java/com/king/oliver/writerschedulegenerator/controllers/SlotController.java
+++ src/main/java/com/king/oliver/writerschedulegenerator/controllers/SlotController.java
@@ -2,9 +2,9 @@ package com.king.oliver.writerschedulegenerator.controllers;
 
 import com.king.oliver.writerschedulegenerator.model.Schedule;
 import com.king.oliver.writerschedulegenerator.model.Slot;
-import com.king.oliver.writerschedulegenerator.model.Writer;
 import com.king.oliver.writerschedulegenerator.services.EditorService;
 import com.king.oliver.writerschedulegenerator.services.ScheduleService;
+import com.king.oliver.writerschedulegenerator.services.SlotService;
 import com.king.oliver.writerschedulegenerator.services.WriterService;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -18,11 +18,13 @@ import javax.validation.Valid;
 @Controller
 public class SlotController {
 
+    private final SlotService slotService;
     private final ScheduleService scheduleService;
     private final WriterService writerService;
     private final EditorService editorService;
 
-    public SlotController(ScheduleService scheduleService, WriterService writerService, EditorService editorService) {
+    public SlotController(SlotService slotService, ScheduleService scheduleService, WriterService writerService, EditorService editorService) {
+        this.slotService = slotService;
         this.scheduleService = scheduleService;
         this.writerService = writerService;
         this.editorService = editorService;
@@ -53,6 +55,41 @@ public class SlotController {
         } else {
             Schedule schedule = scheduleService.findById(scheduleId);
             schedule.addSlot(slot);
+            Schedule saved = scheduleService.save(schedule);
+            return new ModelAndView("redirect:/schedule/" + saved.getId());
+        }
+    }
+
+    @GetMapping("/schedule/{scheduleId}/slot/{slotId}/update")
+    public ModelAndView initUpdateSlot(@PathVariable Long scheduleId, @PathVariable Long slotId) {
+        Schedule schedule = scheduleService.findById(scheduleId);
+        ModelAndView mav = new ModelAndView("slot/update");
+        mav.addObject("slot", slotService.findById(slotId));
+        mav.addObject("schedule", schedule);
+        mav.addObject("writers", writerService.findAll());
+        mav.addObject("editors", editorService.findAll());
+        return mav;
+    }
+
+    @PostMapping("/schedule/{scheduleId}/slot/{slotId}/update")
+    public ModelAndView postUpdateSlot(@Valid Slot slot, BindingResult result, @PathVariable Long scheduleId, @PathVariable Long slotId) {
+        if (result.hasErrors()) {
+            Schedule schedule = scheduleService.findById(scheduleId);
+            ModelAndView mav = new ModelAndView("slot/update");
+            mav.addObject("slot", slot);
+            mav.addObject("schedule", schedule);
+            mav.addObject("writers", writerService.findAll());
+            mav.addObject("editors", editorService.findAll());
+            return mav;
+        } else {
+            Schedule schedule = scheduleService.findById(scheduleId);
+            for (Slot currSlot : schedule.getSlots()) {
+                if (currSlot.getId() == slotId) {
+                    currSlot.setDate(slot.getDate());
+                    currSlot.setWriter(slot.getWriter());
+                    currSlot.setEditor(slot.getEditor());
+                }
+            }
             Schedule saved = scheduleService.save(schedule);
             return new ModelAndView("redirect:/schedule/" + saved.getId());
         }
